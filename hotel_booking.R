fdae1a854bb66c99fe320a9b99e4f15a2c9daf98
Rakshitha Gnanasekaran <rgnanasekaran@uni-koblenz.de>
2022-12-26 18:29:05 +0100

    Create hotel_booking.R
    - Analysing the data frame using R

---
+++ hotel_booking.R
@@ -0,0 +1,52 @@
+install.packages("ggplot2")
+install.packages('tidyverse')
+library(ggplot2)
+library(tidyverse)
+hotel_booking <- read.csv("downloads/hotel_booking.csv")
+
+#To view the hotel data
+#View(hotel_booking)
+
+#hypothesis that people with children have to book in advance.
+ggplot(data = hotel_booking)+
+  geom_point(mapping = aes(x=lead_time, y=children))
+
+#Suggests that guests without children book the most weekend nights. Is this true? 
+ggplot(data = hotel_booking)+ 
+  geom_point(mapping = aes(x=stays_in_weekend_nights, y=children))
+
+#Your stakeholder is interested in developing promotions based on different booking distributions, 
+#but first they need to know how many of the transactions are occurring for each different distribution 
+#type.
+ggplot(data = hotel_booking)+
+  geom_bar(mapping = aes(x = distribution_channel, 
+                         fill=market_segment))+
+  facet_wrap(~deposit_type)+
+  theme(axis.text.x = element_text(angle = 45))
+
+#which market segments generate the largest number of bookings, 
+#and where these bookings are made (city hotels or resort hotels). 
+
+ggplot(data = hotel_booking)+
+  geom_bar(mapping = aes(x = hotel, 
+                         fill = market_segment))+
+  facet_wrap(~market_segment)
+
+#Create a plot that shows the relationship between lead time and guests 
+#traveling with children for online bookings at city hotels
+
+onlineTACityHotels <- hotel_booking %>%
+  filter(hotel=="City Hotel") %>%
+  filter(market_segment=="Online TA")
+ggplot(data = onlineTACityHotels) +
+  geom_point(mapping = aes(x = lead_time, 
+                           y = children,
+                           color = market_segment))
+
+# The plot shows that the bookings with children tend to have a short lead time, 
+# and bookings with 3 children have a much short lead time (<200 days). 
+# So, promotions targeting families can be made closer to the valid booking dates. 
+
+
+
+
